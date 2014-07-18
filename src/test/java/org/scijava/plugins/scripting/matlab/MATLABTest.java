e014d9302f3a018ff1eebc05c7cb7b34b6f04eea
Mark Hiner <hinerm@gmail.com>
2014-07-18 13:06:06 -0500

    Create unit tests
    Added a MATLABTest class to verify basic scripting functionality.
    
    More advanced tests are required to be run from MATLAB itself, and are
    thus more appropriate in other components.

---
+++ src/test/java/org/scijava/plugins/scripting/matlab/MATLABTest.java
@@ -0,0 +1,115 @@
+/*
+ * #%L
+ * MATLAB scripting language plugin.
+ * %%
+ * Copyright (C) 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.plugins.scripting.matlab;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import org.junit.Test;
+import org.scijava.Context;
+import org.scijava.script.ScriptLanguage;
+import org.scijava.script.ScriptModule;
+import org.scijava.script.ScriptService;
+
+/**
+ * MATLAB scripting unit tests (requires a MATLAB installation).
+ *
+ * @author Mark Hiner
+ */
+public class MATLABTest {
+
+	@Test
+	public void testBasic() throws InterruptedException, ExecutionException,
+		IOException, ScriptException
+	{
+		final Context context = new Context(ScriptService.class);
+		final ScriptService scriptService = context.getService(ScriptService.class);
+		final String script = "(1+2)";
+		final ScriptModule m = scriptService.run("add.m", script, true).get();
+		assertTrue(equalDoubleArrays(new double[] { 3.0 }, (double[]) ((Object[]) m
+			.getReturnValue())[0]));
+	}
+
+	@Test
+	public void testBasicMultiline() throws InterruptedException,
+		ExecutionException, IOException, ScriptException
+	{
+		final Context context = new Context(ScriptService.class);
+		final ScriptService scriptService = context.getService(ScriptService.class);
+		final String script = "(1+...\n2)";
+		final ScriptModule m = scriptService.run("add.m", script, true).get();
+		assertTrue(equalDoubleArrays(new double[] { 3.0 }, (double[]) ((Object[]) m
+			.getReturnValue())[0]));
+	}
+
+	@Test
+	public void testLocals() throws ScriptException {
+		final Context context = new Context(ScriptService.class);
+		final ScriptService scriptService = context.getService(ScriptService.class);
+
+		final ScriptLanguage language = scriptService.getLanguageByExtension("m");
+		final ScriptEngine engine = language.getScriptEngine();
+		assertEquals(MATLABScriptEngine.class, engine.getClass());
+		engine.put("hello", 17);
+		final double[] expected = { 17.0 };
+		assertTrue(equalDoubleArrays(expected, (double[]) ((Object[]) engine
+			.eval("hello"))[0]));
+		assertTrue(equalDoubleArrays(expected, (double[]) engine.get("hello")));
+
+		final Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+		bindings.clear();
+		assertEquals(null, bindings.get("hello"));
+	}
+
+	/**
+	 * Helper method to compare two double arrays
+	 *
+	 * @return True iff the two arrays contain equivalent values
+	 */
+	private boolean equalDoubleArrays(final double[] arr1, final double[] arr2) {
+		if (arr1.length != arr2.length) return false;
+
+		for (int i = 0; i < arr1.length; i++) {
+			if (Double.compare(arr1[i], arr2[i]) != 0) return false;
+		}
+
+		return true;
+	}
+}
