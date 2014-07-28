36bbf0f026b281a78be84ba12825285e0e960556
Mark Hiner <hinerm@gmail.com>
2014-07-28 11:01:56 -0500

    Trade MatlabNumericArrays with MATLAB
    If we get a MatlabNumericArray, we will now use the MatlabControl
    library to convert with the underlying MATLAB array type (in both put
    and get directions).

--- src/main/java/org/scijava/plugins/scripting/matlab/MATLABBindings.java
+++ src/main/java/org/scijava/plugins/scripting/matlab/MATLABBindings.java
@@ -41,6 +41,8 @@ import java.util.Set;
 import javax.script.Bindings;
 
 import matlabcontrol.MatlabInvocationException;
+import matlabcontrol.extensions.MatlabNumericArray;
+import matlabcontrol.extensions.MatlabTypeConverter;
 
 /**
  * A {@link Bindings} wrapper around MATLAB's local variables.
@@ -104,7 +106,20 @@ public class MATLABBindings implements Bindings {
 			return value;
 		}
 		catch (final MatlabInvocationException e) {
-			// TODO throw exception/print error -
+
+			// Try special MATLAB data types
+			if (MatlabNumericArray.class.isAssignableFrom(value.getClass())) {
+				// Convert the dataset to a MATLAB array and set it as a local variable
+				// within MATLAB.
+				final MatlabTypeConverter converter =
+					new MatlabTypeConverter(MATLABControlUtils.proxy());
+				try {
+					converter.setNumericArray(name, (MatlabNumericArray) value);
+				}
+				catch (MatlabInvocationException e1) {
+					throw new IllegalArgumentException(e);
+				}
+			}
 		}
 		return null;
 	}
@@ -150,7 +165,19 @@ public class MATLABBindings implements Bindings {
 			v = MATLABControlUtils.proxy().getVariable(k);
 			if (remove) MATLABControlUtils.proxy().eval("clear " + k);
 		}
-		catch (final MatlabInvocationException e) {}
+		catch (final MatlabInvocationException e) {
+
+				// Attempt to retrieve special MATLAB types
+			try {
+				// try recovering key as a MatlabNumericArray
+				final MatlabTypeConverter converter =
+						new MatlabTypeConverter(MATLABControlUtils.proxy());
+				v = converter.getNumericArray(k);
+			}
+			catch (final MatlabInvocationException e1) {
+				System.err.println(e.getStackTrace());
+			}
+		}
 		return v;
 	}
 
