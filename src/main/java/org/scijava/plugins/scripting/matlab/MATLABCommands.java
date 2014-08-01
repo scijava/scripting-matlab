96c2bbcf2ea32144984ae087c73c2f504f0af230
Mark Hiner <hinerm@gmail.com>
2014-08-01 09:43:19 -0500

    Create MATLABCommands interface
    MATLABCommands plugins are singleton plugins for adding utility methods
    to MATLAB.

---
+++ src/main/java/org/scijava/plugins/scripting/matlab/MATLABCommands.java
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * MATLAB scripting language plugin.
+ * %%
+ * Copyright (C) 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package org.scijava.plugins.scripting.matlab;
+
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.RichPlugin;
+import org.scijava.plugin.SingletonPlugin;
+
+
+/**
+ * A suite of commands for use in MATLAB. The {@link MATLABService} will create
+ * variables for each {@link MATLABCommands} using their annotated
+ * {@link Plugin#name()}, allowing convenient access to any of their methods.
+ *
+ * @see MATLABService
+ *
+ * @author Mark Hiner
+ */
+public interface MATLABCommands extends SingletonPlugin, RichPlugin {
+
+	/**
+	 * @return A string describing any commands that should be publicized.
+	 */
+	String usage();
+}
