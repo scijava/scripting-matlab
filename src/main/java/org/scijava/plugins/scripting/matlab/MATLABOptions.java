309567bb9bff33225b7e39c8eb5adab387f76cc9
Curtis Rueden <ctrueden@wisc.edu>
2015-02-11 07:03:25 -0600

    Happy belated New Year 2015

--- src/main/java/org/scijava/plugins/scripting/matlab/MATLABOptions.java
+++ src/main/java/org/scijava/plugins/scripting/matlab/MATLABOptions.java
@@ -1,8 +1,8 @@
 /*
  * #%L
- * ImageJ software for multidimensional image processing and analysis.
+ * MATLAB scripting language plugin.
  * %%
- * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
  * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
  * Institute of Molecular Cell Biology and Genetics.
  * %%
