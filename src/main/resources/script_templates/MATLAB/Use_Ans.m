aa458a841c5d5ac538afb49e556897ae64f73b18
Mark Hiner <hinerm@gmail.com>
2015-08-19 07:48:34 -0500

    Add Use_Ans.m
    This script illustrates how to retrieve the ans variable from MATLAB.

---
+++ src/main/resources/script_templates/MATLAB/Use_Ans.m
@@ -0,0 +1,12 @@
+% @OUTPUT double ans
+
+% This trivial script demonstrates the use of
+% the "ans" variable in the SciJava-MATLAB
+% script language.
+
+% Unassigned statements in MATLAB are automatically
+% assigned to "ans". However, these scripts will not
+% return "ans" unless it is explicitly requested as
+% an output parameter.
+
+0
